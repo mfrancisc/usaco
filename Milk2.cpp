37e4b2201f2d7021faa549ef93d4dfb859236929
Francisc Munteanu <munteanu.francisc@gmail.com>
2016-07-14 22:49:53 +0200

    Milk2 solution

--- Milk2.cpp
+++ Milk2.cpp
@@ -3,48 +3,77 @@ ID: muntean3
 PROG: milk2
 LANG: C++
 */
-
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
+struct Milking {
+    int from;
+    int to;
+};
+
+
+int milkcmp(const void *va, const void *vb) {
+    Milking *a, *b;
+
+    a = (Milking*)va;
+    b = (Milking*)vb;
+
+    if(a->from > b->from)
+        return 1;
+    if(a->from < b->from)
+        return -1;
+    return 0;
+}
 int main() {
 
-	ofstream fout ("milk2.out");
-	ifstream fin ("milk2.in");
+	ofstream fout("milk2.out");
+	ifstream fin("milk2.in");
 
 	int N;
 
 	fin>>N;
 
-	int arr[500][2];
+    Milking milking[5000];
 
 	int from, to;
 	for(int i = 0; i < N; i++) {
 		fin>>from;
 		fin>>to;
 
-		arr[i][0] = from;
-		arr[i][1] = to;
+		milking[i].from = from;
+		milking[i].to  = to;
 	}
 
-	int maxT = arr[0][1] - arr[0][0];
+    // sort by from time
+    qsort(milking, N, sizeof(Milking), milkcmp);
+
+	int maxT = milking[0].to - milking[0].from;
 	int maxI = 0;
 
 	int max = 0;
+    Milking cur = milking[0];
 	for(int i = 1; i < N; i++) {
-		int max = arr[i][1] - arr[i][0];
-		if(maxT < max) maxT = max;
+        if(milking[i].from > cur.to)
+        {
+                max = milking[i].from - cur.to;
+                if(maxI < max) maxI = max;
 
-		if((arr[i-1][0] <= arr[i][0]) && (arr[i][0] <= arr[i-1][1])) {
-			max = arr[i][1] - arr[i-1][0];
-			if(maxT < max) {maxT = max;}
-		} else {
-			max = arr[i][0] - arr[i-1][1];
-			if(maxI < max) {maxI = max;}
-		}
+                max = cur.to - cur.from;
+                if(maxT < max) maxT = max;
+
+                cur = milking[i];
+        } else {
+            if(milking[i].to > cur.to ) {
+                cur.to = milking[i].to;
+            }
+        }
 	}
+
+    // final milking 
+    max = cur.to - cur.from;
+    if(maxT < max) maxT = max;
 
 	fout << maxT << " " << maxI << endl;
 }
