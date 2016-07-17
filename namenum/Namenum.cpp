52dca4721f341023fc3bd4db7ec35cc04af30fba
Francisc Munteanu <munteanu.francisc@gmail.com>
2016-07-17 10:03:30 +0200

    Name the Num simple solution

--- namenum/Namenum.cpp
+++ namenum/Namenum.cpp
@@ -11,17 +11,16 @@ LANG: C++
 
 using namespace std;
 
+char map[256];
 string serialize(string dict_name)
 {
     string ret = "";
     for(int i = 0; i < dict_name.length(); i++) {
-        if(dict_name[i] == 'Q' || dict_name[i] == 'Z'){
+        if(dict_name[i] == 'Q' || dict_name[i] == 'Z')
             return "1";
-        } else if( dict_name[i] < 'Q') {
-            ret += ((dict_name[i] - 'A') / 3) + '2'; 
-        } else {
-            ret += ((dict_name[i] - 'Q') / 3) + '7'; 
-        }
+
+        char c = map[dict_name[i]]; 
+        ret += c;
     }
 
     return ret;
@@ -29,6 +28,14 @@ string serialize(string dict_name)
 
 }
 int main() {
+map['A'] = map['B'] = map['C'] = '2';
+map['D'] = map['E'] = map['F'] = '3';
+map['G'] = map['H'] = map['I'] = '4';
+map['J'] = map['K'] = map['L'] = '5';
+map['M'] = map['N'] = map['O'] = '6';
+map['P'] = map['R'] = map['S'] = '7';
+map['T'] = map['U'] = map['V'] = '8';
+map['W'] = map['X'] = map['Y'] = '9';
 
     ofstream fout ("namenum.out");
     ifstream fin ("namenum.in"), fdin ("dict.txt");
